9eecaad27b149ef6bafcbe4b93fc2827c60cbf33
Yaman <yash.saini01230@gmail.com>
2023-11-24 13:14:15 +0530

    video24 complete replace arguments

---
+++ app/code/Video23/ReplaceConstructorArguments/Controller/Index/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Video23\ReplaceConstructorArguments\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements ActionInterface {
+    protected $pageFactory;
+
+    public function __construct(PageFactory $pageFactory) {
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute() {
+        $resultPage = $this->pageFactory->create();
+        return $resultPage;
+    }
+}
---
+++ app/code/Video23/ReplaceConstructorArguments/Model/DefaultName.php
@@ -0,0 +1,8 @@
+<?php
+namespace Video23\ReplaceConstructorArguments\Model;
+
+class DefaultName   {
+    public function getName() {
+        return "training replace constructor arguments";
+    }
+}
